b06a487b06fe32a02d4e72c56c9373e104c108ce
Nafisa-Tasnum <nafisatasnum27@gmail.com>
2024-12-24 19:51:44 +0600

    styling

--- src/components/FoodItem.tsx
+++ src/components/FoodItem.tsx
@@ -6,35 +6,28 @@ const FoodItem =({src, name, description, rating, location}:{
     location?:string;
 })=>{
  return <div
-    className="w-full rounded-xl bg-gradient-to-tr from-[#FF512F] to-[#F09819] p-6 shadow-xl sm:w-1/2 sm:p-8"
+    className="w-full rounded-xl bg-gradient-to-tr from-[#FF512F] to-[#F09819] p-6 shadow-xl sm:w-2/3 sm:p-9"
   >
     <div
       className="mb-4 flex flex-col items-start justify-between gap-4 lg:flex-row"
     >
       <div>
         <img src={src}/>
-        <h3 className="text-2xl font-bold text-neutral-100 sm:text-3xl">
-         felix is awesome
+        <h3 className="text-3xl font-bold text-neutral-100 sm:text-5xl">
+         {name}
         </h3>
-        <p className="text-orange-200">hello world</p>
+        
       </div>
 
-      <span
-        className="order-first inline-block rounded-full bg-orange-200 bg-opacity-50 px-3 py-1 text-xs font-bold uppercase tracking-wider text-center text-orange-600 lg:order-none"
-        >hello world</span
-      >
     </div>
 
     <div className="mb-4">
-      <span className="text-6xl font-bold text-neutral-100"
-        >hello world</span
-      >
+      
       <span className="text-lg font-bold text-orange-100"
-        >hello world</span
+        >{description}</span
       >
-      <span className="ms-3 text-orange-200"
-        >hello world</span
-      >
+      <p className="text-xl text-orange-200">{location}</p>
+      
     </div>
   </div>
 }
