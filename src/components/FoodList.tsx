6f4270ef85744fb653b9846bf76f57b7beaa5d0f
Nafisa-Tasnum <nafisatasnum27@gmail.com>
2024-12-24 19:31:57 +0600

    fixed types

--- src/components/FoodList.tsx
+++ src/components/FoodList.tsx
@@ -2,12 +2,14 @@ import data from "@/data_files/data.json"
 import image from "@/images/dishes/1117a963-dd82-4458-bcad-8f4d77a2586e.jpg"
 import { FoodItem } from "./FoodItem";
 
-const images = import.meta.glob('../images/dishes/*', { eager: true });
+interface ImageModule { default: { src: string; width: number; height: number; format: string; orientation: number; }; }
+
+const images:Record<string, ImageModule> = import.meta.glob('../images/dishes/*', { eager: true });
 const FoodList = ()=>{
     return <div>
         {data.dishes.map((values)=>{
             const searchableFileString = `../images/dishes/${values.id}.jpg`
-            const src = images[searchableFileString].default.src
+            const src = images[searchableFileString]?.default?.src
             console.log(src)
             return <FoodItem {...values}/>
         })}
