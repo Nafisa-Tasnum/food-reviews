cc4f7e86c277ee4e64a4b6bf5b0b4f2838539dd2
Nafisa-Tasnum <nafisatasnum27@gmail.com>
2024-12-24 19:25:48 +0600

    matched id with source and got source in return

--- src/components/FoodItem.tsx
+++ src/components/FoodItem.tsx
@@ -1,5 +1,36 @@
-const FoodItem =({id, name, description, rating, location})=>{
- return
+const FoodItem =({src, name, description, rating, location})=>{
+ return <div
+    className="w-full rounded-xl bg-gradient-to-tr from-[#FF512F] to-[#F09819] p-6 shadow-xl sm:w-1/2 sm:p-8"
+  >
+    <div
+      className="mb-4 flex flex-col items-start justify-between gap-4 lg:flex-row"
+    >
+      <div>
+        <img src={src}/>
+        <h3 className="text-2xl font-bold text-neutral-100 sm:text-3xl">
+         felix is awesome
+        </h3>
+        <p className="text-orange-200">hello world</p>
+      </div>
+
+      <span
+        className="order-first inline-block rounded-full bg-orange-200 bg-opacity-50 px-3 py-1 text-xs font-bold uppercase tracking-wider text-center text-orange-600 lg:order-none"
+        >hello world</span
+      >
+    </div>
+
+    <div className="mb-4">
+      <span className="text-6xl font-bold text-neutral-100"
+        >hello world</span
+      >
+      <span className="text-lg font-bold text-orange-100"
+        >hello world</span
+      >
+      <span className="ms-3 text-orange-200"
+        >hello world</span
+      >
+    </div>
+  </div>
 }
 
 export {FoodItem}
--- src/components/FoodList.tsx
+++ src/components/FoodList.tsx
@@ -1,41 +1,17 @@
 import data from "@/data_files/data.json"
 import image from "@/images/dishes/1117a963-dd82-4458-bcad-8f4d77a2586e.jpg"
+import { FoodItem } from "./FoodItem";
 
 const images = import.meta.glob('../images/dishes/*', { eager: true });
 const FoodList = ()=>{
-    console.log(images)
-    return <div
-    className="w-full rounded-xl bg-gradient-to-tr from-[#FF512F] to-[#F09819] p-6 shadow-xl sm:w-1/2 sm:p-8"
-  >
-    <div
-      className="mb-4 flex flex-col items-start justify-between gap-4 lg:flex-row"
-    >
-      <div>
-        <img src={image.src}/>
-        <h3 className="text-2xl font-bold text-neutral-100 sm:text-3xl">
-         felix is awesome
-        </h3>
-        <p className="text-orange-200">hello world</p>
-      </div>
-
-      <span
-        className="order-first inline-block rounded-full bg-orange-200 bg-opacity-50 px-3 py-1 text-xs font-bold uppercase tracking-wider text-center text-orange-600 lg:order-none"
-        >hello world</span
-      >
-    </div>
-
-    <div className="mb-4">
-      <span className="text-6xl font-bold text-neutral-100"
-        >hello world</span
-      >
-      <span className="text-lg font-bold text-orange-100"
-        >hello world</span
-      >
-      <span className="ms-3 text-orange-200"
-        >hello world</span
-      >
-    </div>
-  </div>
+    return <div>
+        {data.dishes.map((values)=>{
+            const searchableFileString = `../images/dishes/${values.id}.jpg`
+            const src = images[searchableFileString].default.src
+            console.log(src)
+            return <FoodItem {...values}/>
+        })}
+        </div>
 }
 
 export {FoodList}
